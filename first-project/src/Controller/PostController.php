9af0727250d5f95d342446bfc461a8d9c446c59a
BoomPowBep <37978668+BoomPowbep@users.noreply.github.com>
2019-01-21 11:22:49 +0100

    Post en param

--- first-project/src/Controller/PostController.php
+++ first-project/src/Controller/PostController.php
@@ -9,12 +9,21 @@
 namespace App\Controller;
 
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     public function indexAction()
     {
-        return $this->render("Post/index.html.twig");
+        $post = new Post();
+        $post->setTitle("Les pinguins d'Alaska");
+
+        $params = [
+            "toto" => "Salut",
+            "post" => $post
+        ];
+
+        return $this->render("Post/index.html.twig", $params);
     }
 }
